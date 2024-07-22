4dfdebb70ced469edc03fe791aa99c6df95191a6
H3nry-T <ht3drex@gmail.com>
2024-07-22 19:13:27 +0100

    adds launch.json and created main page

---
+++ src/app/recursive/page.tsx
@@ -0,0 +1,7 @@
+export const RecursivePage: (props: {element: React.ReactNode}) => React.ReactElement = () => {
+    return (
+        <div className="flex items-center">
+            filename &rarr; 
+        </div>
+    )
+}
