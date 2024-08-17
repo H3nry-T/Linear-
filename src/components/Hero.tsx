9fc53514abf727ea37c89dbf09facf1801f6e899
H3nry-T <ht3drex@gmail.com>
2024-08-17 19:51:19 +0100

    beams added

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,8 +3,12 @@ import { Spotlight } from "./ui/Spotlight";
 
 export default function Hero() {
   return (
-    <section>
-      <Spotlight fill="black" />
+    <section className="pb-20 pt-36">
+      <div>
+        <Spotlight fill="white" className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"/>
+        <Spotlight fill="purple" className="top-10 left-full h-[80vh] w-[50vw]"/>
+        <Spotlight fill="blue" className="top-28 left-80 h-[80vh] w-[50vw]"/>
+      </div>
     </section>
   );
 }
