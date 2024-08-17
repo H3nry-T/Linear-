266a50dbc28d42afef98d00ff5dac83264b7df8d
H3nry-T <ht3drex@gmail.com>
2024-08-17 18:55:33 +0100

    added spotlight

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
-import Link from "next/link";
+import Hero from '@/components/Hero';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      hello world: <br />
-      <Link href="/recursive">link to the folder recursive react component</Link>
+    <main className="bg-black-100 flex flex-col justify-center items-center overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <Hero />
+      </div>
     </main>
   );
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Spotlight } from "./ui/Spotlight";
+
+export default function Hero() {
+  return (
+    <section>
+      <Spotlight fill="black" />
+    </section>
+  );
+}
