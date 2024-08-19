98b959ac171195f24226e417c6bf0c6a9051a25b
H3nry-T <ht3drex@gmail.com>
2024-08-19 16:31:17 +0100

    added Grid background

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Spotlight } from "./ui/Spotlight";
+import { GridBackground } from "./ui/GridBackground";
 
 export default function Hero() {
   return (
@@ -9,6 +10,7 @@ export default function Hero() {
         <Spotlight fill="purple" className="top-10 left-full h-[80vh] w-[50vw]"/>
         <Spotlight fill="blue" className="top-28 left-80 h-[80vh] w-[50vw]"/>
       </div>
+      <GridBackground/>
     </section>
   );
 }
