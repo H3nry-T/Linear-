a3b1500041209e1692e30ec130dcd074a81bc89f
h3nry_t <ht3drex@gmail.com>
2024-08-20 13:01:15 +0100

    took out left spotlight

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,7 +11,7 @@ export default function Hero() {
     <section className="pb-20 pt-36">
       <div>
         <Spotlight fill="white" className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"/>
-        <Spotlight fill="purple" className="top-10 left-full h-[80vh] w-[50vw]"/>
+        {/* <Spotlight fill="purple" className="top-10 left-full h-[80vh] w-[50vw]"/> */}
         <Spotlight fill="blue" className="top-28 left-80 h-[80vh] w-[50vw]"/>
       </div>
       <GridBackground/>
