dbbf189fde92ff428c8fda519ce5def8f9d610ed
h3nry_t <ht3drex@gmail.com>
2024-08-20 15:22:46 +0100

    added a bentobox grid section

---
+++ src/components/Grid.tsx
@@ -0,0 +1,29 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+import {
+  IconArrowWaveRightUp,
+  IconBoxAlignRightFilled,
+  IconBoxAlignTopLeft,
+  IconClipboardCopy,
+  IconFileBroken,
+  IconSignature,
+  IconTableColumn,
+} from "@tabler/icons-react";
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
+
+export function Grid({items, className}: {items: {title: string, description: string, header: React.ReactNode, icon: React.ReactNode}[], className?: string}) {
+  return (
+    <BentoGrid className={ cn( "max-w-4xl mx-auto", className ) }>
+      {items.map((item, i) => (
+        <BentoGridItem
+          key={i}
+          title={item.title}
+          description={item.description}
+          header={item.header}
+          icon={item.icon}
+          className={i === 3 || i === 6 ? "md:col-span-2" : ""}
+        />
+      ))}
+    </BentoGrid>
+  );
+}
