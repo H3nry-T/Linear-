c4b71f55aa1459862939f7ec67bf983087cae100
h3nry_t <ht3drex@gmail.com>
2024-08-21 13:30:49 +0100

    bento box section pushed down

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,7 +8,7 @@ import { CiLocationArrow1 } from "react-icons/ci";
 
 export default function Hero() {
   return (
-    <section className="pb-20 pt-36">
+    <section className="pb-20 pt-36 min-h-screen">
       <div>
         <Spotlight fill="white" className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"/>
         {/* <Spotlight fill="purple" className="top-10 left-full h-[80vh] w-[50vw]"/> */}
