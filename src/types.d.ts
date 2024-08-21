0c8586b96621cbae65f18c7462b38b44fb17479e
h3nry_t <ht3drex@gmail.com>
2024-08-21 13:27:44 +0100

    GridItem type

---
+++ src/types.d.ts
@@ -0,0 +1,10 @@
+type GridItem = {
+  id: number;
+  title: string;
+  description: string;
+  className: string;
+  imgClassName: string;
+  titleClassName: string;
+  img: string;
+  spareImg: string;
+};
