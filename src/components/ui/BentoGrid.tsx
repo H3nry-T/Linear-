2a12be145e53f61aea422e893775ea5f53e4f5c6
h3nry_t <ht3drex@gmail.com>
2024-08-22 10:42:47 +0100

    Update BentoGrid component with new title styles and gradient animations

--- src/components/ui/BentoGrid.tsx
+++ src/components/ui/BentoGrid.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/lib/utils";
+import { BackgroundGradientAnimation } from "./GradientBackground";
 
 export const BentoGrid = ({
   className,
@@ -41,7 +42,7 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        "row-span-1 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4 relative overflow-hidden",
+        "row-span-1 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col gap-4 relative overflow-hidden",
         className
       )}
       style={{
@@ -52,18 +53,17 @@ export const BentoGridItem = ({
         <div className='w-full h-full absolute left-0 top-0'>
           {img && <img src={img} alt={img} className={cn(imgClassName, 'object-cover object-center')}/>}
         </div>
-        <div className={id === 5 ? `absolute right-0 -bottom-5` : ''}>
-
-        </div>
       </div>
-      <div className="group-hover/bento:translate-x-2 transition duration-200">
-        <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2 relative">
-          {title}
-        </div>
-        <div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">
-          {description}
-        </div>
+      <div className={`absolute right-0 -bottom-5 ${id === 5 && 'w-full opacity-80'}`}>
+        {spareImg && <img src={spareImg} alt={spareImg} className={cn('object-cover object-center w-full h-full')}/>}
       </div>
+      {id === 6 && (<BackgroundGradientAnimation containerClassName="absolute w-full h-full inset-0">
+        <div className="flex justify-center items-center text-white font-bold"/>
+      </BackgroundGradientAnimation>)}
+      <div className={ cn('absolute inset-0 w-full h-full bg-gray-500/0 group-hover/bento:bg-gray-500/20 transition-all ease-in-out') }></div>
+      <h2 className={cn(titleClassName, 'relative text-white font-semibold text-lg transition-transform ease-in-out md:h-full min-h-40 flex flex-col p-5 lg:p-10')}>
+        {title}
+      </h2>
     </div>
   );
 };
